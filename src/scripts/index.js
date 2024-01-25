e09225d6f84631add4a80a8df6dfda2407198287
ridho-dev <ridhopardede.dev@gmail.com>
2024-01-25 14:31:53 +0700

    Add gradient background

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime'
 import '../styles/style.css'
 import '../styles/responsive.css'
+import '../styles/bg.css'
 import App from './views/app'
 
 // eslint-disable-next-line no-unused-vars
