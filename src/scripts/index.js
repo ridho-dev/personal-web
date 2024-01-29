179bae36c23a35494c0362a8a3788c749709dea3
ridho-dev <ridhopardede.dev@gmail.com>
2024-01-29 20:37:05 +0700

    Modify Navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,6 @@
 import 'regenerator-runtime'
 import '../styles/style.css'
 import '../styles/responsive.css'
-import '../styles/bg.css'
 import App from './views/app'
 
 // eslint-disable-next-line no-unused-vars
