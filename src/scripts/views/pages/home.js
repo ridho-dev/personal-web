f9e729de310c2e26a061c4d3f525600101c1cd8f
ridho-dev <ridhopardede.dev@gmail.com>
2024-01-30 10:41:44 +0700

    Add home content

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,9 +1,13 @@
 const HomePage = {
   async render () {
     return `
-      <section>
-        <p>Hi, I'm</p>
-        <h2>Ridho Pardede</h2>
+      <section class="home-section">
+        <div class="greetings">
+          <p>Hi there! I'm</p>
+          <h1>Ridho Pardede</h1>
+          <p class="home-about">I'm passionate about crafting experiences that are engaging, accessible, and user-centric.</p>
+        </div>
+        
       </section>
       
     `
